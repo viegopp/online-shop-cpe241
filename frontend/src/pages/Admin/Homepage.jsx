9e2738183813c2c368ec6d68c83f68455b175c08
Mudev <muaykillz2548@gmail.com>
2025-05-02 08:13:31 +0700

    ✨ feat(app): add home page

--- frontend/src/pages/Admin/Homepage.jsx
+++ frontend/src/pages/Admin/Homepage.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import StockManageTable from "../../components/tables/StockManageTable";
-import Breadcrumbs from "../../components/ui/Breadcrumbs";
+import MainLayout from "../../components/layouts/MainLayout";
 
 const Homepage = () => {
   const breadcrumbItems = [
-    { label: "Home", path: "/admin" },
-    { label: "Stock Management", path: "/admin/stock-management" },
+    { label: "Home", href: "/" },
+    // { label: "Dashboard", href: "/dashboard" },
   ];
 
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-2xl font-bold text-slate-900">Dashboard</h1>
-
-      <Breadcrumbs items={breadcrumbItems} title="Stock Management" />
-
-      <StockManageTable />
-    </div>
+    <MainLayout breadcrumbs={breadcrumbItems}>
+      <div className="flex justify-center items-center flex-col w-full h-full min-h-[400px]">
+        {/* Logo */}
+        <div className="flex items-center justify-center gap-3 px-4 py-8 opacity-20">
+          <img src="/logo.svg" alt="OnlineShop Logo" className="w-20 h-20" />
+          <h1 className="text-6xl font-bold leading-none text-slate-900 max-md:text-4xl">
+            OnlineShop
+          </h1>
+        </div>
+      </div>
+    </MainLayout>
   );
 };
 
